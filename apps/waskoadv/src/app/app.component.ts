3ef1825129ad3a80ed0286ae8cb3377d4248ed82
dawbit <dawbitner@gmail.com>
2021-06-28 08:50:57 +0200

    initial commit

---
+++ apps/waskoadv/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'wasko-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'waskoadv';
+}
